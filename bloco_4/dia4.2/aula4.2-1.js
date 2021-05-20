e26d7ae7538710c84072f461761f3ad92fa6c136
Felipe Dias <felipeferreiradias1@gmail.com>
2021-05-20 15:58:26 -0300

    adiciona exercicios da aula 4.2

---
+++ bloco_4/dia4.2/aula4.2-1.js
@@ -0,0 +1,26 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length);
+// 3
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+// Brincar com o cachorro
+
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+console.log(tasksList);
+
+tasksList.unshift('Fazer exercícios da Trybe');  // adiciona mais uma tarefa ao início do Array
+console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+tasksList.pop();  // remove a última tarefa
+console.log(tasksList);
+
+let indexOfTask = tasksList.indexOf('Reunião'); // encontra posição do 'Reunião' no array
+console.log(indexOfTask);
