29ff735a19fd207ca70b255413febba32da14746
Felipe Dias <felipeferreiradias1@gmail.com>
2021-06-01 15:12:03 -0300

    Adiciona parte I dos exercicios do dia 5.3

--- bloco_5/dia5.3/script.js
+++ bloco_5/dia5.3/script.js
@@ -4,32 +4,70 @@ const divUm = document.getElementById('divUm');
 const divDois = document.getElementById('divDois');
 const divTres = document.getElementById('divTres');
 const input = document.getElementById('input');
-const myWebpage = document.getElementById('mySpotrybefy');
-
+const myWebPage = document.getElementById('mySpotrybefy');
 
 //1. Copie esse arquivo e edite apenas ele;
 
 //2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-divContentOne = document.querySelector('#divUm');
-divContentOne.addEventListener("mouseover", selected);
-
-function selected(string) {    
-    string.class = "tech";
-}
 //2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+divUm.addEventListener("click", select);
+divDois.addEventListener("click", select);
+divTres.addEventListener("click", select);
 
-
-
+function select(evento) {    
+  
+  if (evento.target == divUm) {
+    divDois.className = "";
+    divTres.className = "";
+    divUm.className = "tech";
+  } else if (evento.target == divDois) {
+    divUm.className = "";
+    divTres.className = "";
+    divDois.className = "tech";
+  } else {
+    divUm.className = "";
+    divDois.className = "";
+    divTres.className = "tech";   
+  }
+}
 
 //3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 //com a classe 'tech';
+
+divUm.addEventListener("click", textChange);
+divDois.addEventListener("click", textChange);
+divTres.addEventListener("click", textChange);
+
+function textChange (evento) {
+  if (evento.target.className == "tech") {
+    evento.target.innerText = input.value;
+  }
+}
 
 //4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 //redirecione para alguma página;
 //4.1. Que tal redirecionar para seu portifólio?
 
+myWebPage.addEventListener("dblclick", directly);
+
+function directly (){
+  window.location.replace("https://felipedias1.github.io/");
+}
+
 //5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 //a cor do mesmo;
+
+myWebPage.addEventListener("mouseover", changecolor);
+myWebPage.addEventListener("mouseout", backcolor);
+
+function changecolor (){
+  myWebPage.style.color = "purple"
+}
+
+function backcolor (){
+  myWebPage.style.color = "white"
+}
+
 
 //Segue abaixo um exemplo do uso de event.target:
 
