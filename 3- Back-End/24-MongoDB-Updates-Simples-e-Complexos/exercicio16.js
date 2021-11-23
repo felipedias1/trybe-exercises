85f1260a3b680aab78d92f94eb42d220bbe9ee02
Felipe Dias <felipeferreiradias1@gmail.com>
2021-11-23 12:06:47 -0300

    add exercises 24.1 - part 2

---
+++ 3- Back-End/24-MongoDB-Updates-Simples-e-Complexos/exercicio16.js
@@ -0,0 +1,10 @@
+//Exercício 16 : Produza uma query onde os mutantes class 
+// omega ou gama passam a ter seu poder de 500 somente se 
+// seu poder for menor que 500.
+db.xmen.updateMany(
+  {class: { $in: ["omega","gama"] } },
+  {
+    $currentDate: { lastUpdate: { $type: "timestamp" } },
+    $max: { power: 500 }
+  }
+)
---
+++ 3- Back-End/24-MongoDB-Updates-Simples-e-Complexos/exercicio19.js
@@ -0,0 +1,22 @@
+// Exercício 19 : Em apenas uma query adicione o campo 
+// areas com o seguinte array como valor: ["Students Room"] 
+// a todos os mutantes que são Senior Staff que tenham 
+// poder acima de 100 e para todos os Junior Staff com 
+// poder acima de 200.
+db.xmen.updateMany(
+  {$or:[
+      { 
+        "occupation": "Senior Staff", 
+        power: { $gt: 100 },
+      },
+      {
+        "occupation": "Junior Staff",
+        power: { $gt: 200 }
+      },
+    ]
+  },  
+  { 
+    $currentDate: { lastUpdate: { $type: "timestamp" } },
+    $set: { areas: ["Students Room"] }
+  }
+)
